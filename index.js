e6aab9a471263d263b5d9bd2cae19d14d2a9b45c
rauatorres <rauatorres@gmail.com>
2024-12-18 15:11:54 -0300

    configurando a porta do server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./configs/server');
 
 require('dotenv').config();
 
-const port = process.env.SERVER_PORT;
+const port = process.env.SERVER_PORT || 3000;
 
 app.listen(port, ()=>{
     console.log(`App rodando na porta ${port}`);
